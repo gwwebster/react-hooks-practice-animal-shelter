3e4378112de6a7c9c2e698b6367d676f602a9a4c
George <gwwebster23@gmail.com>
2023-07-03 10:24:07 -0500

    Animal Shelter

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
 import React, { useState } from "react";
-
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
 
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
+
+
+  function handleChangeType(e) {
+    setFilters({ type: e.target.value });
+  }
+
+  function handleFindPets() {
+    fetch(filters.type === "all" ? "http://localhost:3001/pets?" :
+    "http://localhost:3001/pets?" + new URLSearchParams({ type: filters.type }))
+    .then(r => r.json())
+    .then(data => setPets(data));
+  }
+
+  function handleAdoptPet(id) {
+    setPets(pets.map(pet => (
+      pet.id === id ? {...pet, isAdopted: true} : pet
+    )));
+  }
 
   return (
     <div className="ui container">
@@ -15,10 +32,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={handleChangeType} onFindPetsClick={handleFindPets} />
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={handleAdoptPet} />
           </div>
         </div>
       </div>
